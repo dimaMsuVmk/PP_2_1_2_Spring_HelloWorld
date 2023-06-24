670757cdd222f2f64c3303d43ab49d05d701141e
dimaVmkMSU <mega.dima.r@mail.ru>
2023-06-24 22:32:34 +0300

    task 2.1.3

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,12 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld beanHelloWorld_1 = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld beanHelloWorld_2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(beanHelloWorld_1 == beanHelloWorld_2);
+        //System.out.println(beanHelloWorld_1.getMessage());
+        Cat one = applicationContext.getBean(Cat.class);
+        Cat two = applicationContext.getBean(Cat.class);
+        System.out.println(one == two);
     }
 }
